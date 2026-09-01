71d43717288a9666bd5d9c018ff1d589b49e28c1
hungmid <135576020+hungmid@users.noreply.github.com>
2026-09-01 13:36:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/affmebe/',
+    base: '/affsuan/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
