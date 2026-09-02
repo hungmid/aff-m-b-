17c6abce7ae53cf2a4aefdbee4058d6479cc7e77
hungmid <135576020+hungmid@users.noreply.github.com>
2026-09-02 11:40:36 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://afnakrhjcwljamiigodg.supabase.co'
-const supabaseKey = 'sb_publishable_i5oDuhmBbQ7MSlhINyVDsA_8Tu3Bckx'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmbmFrcmhqY3dsamFtaWlnb2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzMDcwNzMsImV4cCI6MjEwMzg4MzA3M30.eFOT2Mn9G-9SEnn9vZqt0Ws0vDf8blrK7L-sz75u4xA'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
